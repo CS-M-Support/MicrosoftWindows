4914d5cce06122f5f4129d6cd593c862d8b5aa2f
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-04-26 10:23:53 +0200

    Create umgehen der ständigen passwortaufforderung bei microsoft 365 konten.ps1

---
+++ Outlook/umgehen der ständigen passwortaufforderung bei microsoft 365 konten.ps1
@@ -0,0 +1,6 @@
+# Wenn man Microsoft 365 verwendet, kann es oft vorkommen, dass Outlook immer wieder nach einem Passwort fragt. Dies liegt daran, dass Outlook teils die Legacy Authentifizierungsmethode verwendet.
+# Mit diesem Script wird ein neuer Registry-Key erstellt, welcher Outlook zur modernen Authentifizierungsmethode zwingt.
+# Basiert auf diesen Troubleshoot von Microsoft: https://docs.microsoft.com/en-us/outlook/troubleshoot/authentication/outlook-prompt-password-modern-authentication-enabled
+
+# Neuer Registry-Key erstellen
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Exchange" -Name "AlwaysUseMSOAuthForAutoDiscover" -Value ”1”  -PropertyType "DWord"
